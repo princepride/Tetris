8eaa6dca757426080bb8ff7d166a4b5f20ddb241
汪志鹏 <13476859295@163.com>
2023-03-03 21:59:48 +0800

    finished

--- script.js
+++ script.js
@@ -2,27 +2,18 @@ const ROW = 22;
 const COLUMN = 10;
 let container = document.querySelector('#container');
 let ground = document.createElement('div'); 
-let centrePoint = [2, 4]
-
-ground.className = 'ground';
-for(let i = 0; i < ROW; i++) {
-    let line = document.createElement('div'); 
-    line.className = 'line';
-    line.id = 'line'+i;
-    for(let j = 0; j < COLUMN; j++) {
-        let node = document.createElement('div');
-        node.className = 'node';
-        if(i<2){
-            node.classList.add('norender');
-        }
-        node.id = 'node'+i+'-'+j;
-        line.appendChild(node);
-    }
-    ground.appendChild(line);
-}
-container.appendChild(ground);
-// centre point (1, 4)
+let centrePoint = []
+let moveable = [];
 let type = 0;
+let arr = [];
+for (let i = 0; i < ROW; i++) {
+  arr[i] = [];
+  for (let j = 0; j < COLUMN; j++) {
+    arr[i][j] = false;
+  }
+}
+
+
 const generateTetris = (x, y) => {
     type = Math.random()*7;
     //长条
@@ -90,6 +81,8 @@ const generateTetris = (x, y) => {
     }
 }
 
+
+
 const rotate_point_around_center = (x, y, moveable, clockwise=true) => {
     return moveable.map((item)=>{
         //计算相对于中心点的偏移量
@@ -106,13 +99,6 @@ const rotate_point_around_center = (x, y, moveable, clockwise=true) => {
     })
 }
 
-const arr = [];
-for (let i = 0; i < ROW; i++) {
-  arr[i] = [];
-  for (let j = 0; j < COLUMN; j++) {
-    arr[i][j] = false;
-  }
-}
 const render = () => {
     for(let i = 0; i < ROW; i++) {
         for(let j = 0; j < COLUMN; j++) {
@@ -124,6 +110,73 @@ const render = () => {
     }
 }
 
+const tetrisAction = (x, y, action) => {
+    // let tempMoveable = moveable.map((item)=>[item[0]+x,item[1]+y])
+    let tempMoveable = action()
+    if (tempMoveable.every(item => 
+        item[0] < ROW && 
+        (moveable.some((subArray) => {
+            return JSON.stringify(subArray) === JSON.stringify(item);
+        }) || 
+        arr[item[0]][item[1]] === false)
+        )){
+        if (tempMoveable.every(item => item[1] >= 0 && item[1] < COLUMN)) {
+            moveable.forEach((item)=>{arr[item[0]][item[1]] = false})
+            tempMoveable.forEach((item)=>{arr[item[0]][item[1]] = true})
+            centrePoint = [centrePoint[0]+x,centrePoint[1]+y];
+            moveable = tempMoveable;
+            render()
+        }
+        return false;
+    }
+    else {
+        // moveable.forEach((item)=>{arr[item[0]][item[1]] = false})
+        return true;
+    }
+}
+const init = () => {
+    centrePoint = [2, 4];
+    moveable = generateTetris(centrePoint[0],centrePoint[1]);
+    moveable.forEach(element => {
+        arr[element[0]][element[1]] = true;
+    });
+    render()
+}
+
+const deleteAllTrue = () => {
+    let tempArray = [];
+    let count = 0;
+    for(let i = 0; i < ROW; i++) {
+        if(arr[i].every(item=>item===true)) {
+            count++;
+        }
+        else {
+            tempArray.push(arr[i]);
+        }
+    }
+    tempArray.unshift(...new Array(count).fill(null).map(() => new Array(COLUMN).fill(false)));
+    arr = tempArray;
+    init();
+}
+
+ground.className = 'ground';
+for(let i = 0; i < ROW; i++) {
+    let line = document.createElement('div'); 
+    line.className = 'line';
+    line.id = 'line'+i;
+    for(let j = 0; j < COLUMN; j++) {
+        let node = document.createElement('div');
+        node.className = 'node';
+        if(i<2){
+            node.classList.add('norender');
+        }
+        node.id = 'node'+i+'-'+j;
+        line.appendChild(node);
+    }
+    ground.appendChild(line);
+}
+container.appendChild(ground);
+
 document.addEventListener('keydown', (event) => {
     //space 32
     //A 65
@@ -134,30 +187,25 @@ document.addEventListener('keydown', (event) => {
         
     }
     else if(event.keyCode === 65) {
-        centrePoint = [centrePoint[0],centrePoint[1]-1]
-        moveable = moveable.map((item)=>[item[0],item[1]])
+        tetrisAction(0, -1, ()=>moveable.map((item) => {return [item[0],item[1]-1]}));
     }
     else if(event.keyCode === 68) {
-        centrePoint = [centrePoint[0],centrePoint[1]+1]
+        tetrisAction(0, 1, ()=>moveable.map((item) => {return [item[0],item[1]+1]}));
     }
+    // 顺时针旋转90度
     else if(event.keyCode === 87) {
-
+        tetrisAction(0 ,0 ,()=>rotate_point_around_center(centrePoint[0], centrePoint[1], moveable, clockwise=true))
     }
+    // 逆时针旋转90度
     else if(event.keyCode === 83) {
-
+        tetrisAction(0 ,0 ,()=>rotate_point_around_center(centrePoint[0], centrePoint[1], moveable, clockwise=false))
     }
 })
 
-let moveable = generateTetris(centrePoint[0],centrePoint[1]);
-moveable.forEach(element => {
-    arr[element[0]][element[1]] = true;
-});
-render()
+init();
+
 setInterval(() => {
-    let tempMoveable = moveable.map((item)=>[item[0]+1,item[1]])
-    moveable.forEach((item)=>{arr[item[0]][item[1]] = false})
-    tempMoveable.forEach((item)=>{arr[item[0]][item[1]] = true})
-    centrePoint = [centrePoint[0]+1,centrePoint[1]]
-    render()
-    moveable = tempMoveable;
+    if(tetrisAction(1, 0, ()=>moveable.map((item) => {return [item[0]+1,item[1]]}))) {
+        deleteAllTrue();
+    }
 }, 500);
